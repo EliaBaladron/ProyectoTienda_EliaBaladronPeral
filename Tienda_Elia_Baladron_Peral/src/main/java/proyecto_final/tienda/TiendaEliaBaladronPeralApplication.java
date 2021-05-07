b4c26c68ae9513d0349e5039d2001537e50625b3
EliaBaladron <82671064+EliaBaladron@users.noreply.github.com>
2021-05-07 12:58:34 +0200

    Proyecto creado

---
+++ Tienda_Elia_Baladron_Peral/src/main/java/proyecto_final/tienda/TiendaEliaBaladronPeralApplication.java
@@ -0,0 +1,13 @@
+package proyecto_final.tienda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaEliaBaladronPeralApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendaEliaBaladronPeralApplication.class, args);
+	}
+
+}
---
+++ Tienda_Elia_Baladron_Peral/src/test/java/proyecto_final/tienda/TiendaEliaBaladronPeralApplicationTests.java
@@ -0,0 +1,13 @@
+package proyecto_final.tienda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendaEliaBaladronPeralApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
