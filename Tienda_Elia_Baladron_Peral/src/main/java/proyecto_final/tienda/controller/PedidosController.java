8928c6e110cd095f15f4b6a0911e8da0cab51751
EliaBaladron <82671064+EliaBaladron@users.noreply.github.com>
2021-05-10 11:06:16 +0200

    Controller, Service y DAO de Pedidos

---
+++ Tienda_Elia_Baladron_Peral/src/main/java/proyecto_final/tienda/controller/PedidosController.java
@@ -0,0 +1,58 @@
+package proyecto_final.tienda.controller;
+
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import proyecto_final.tienda.model.Pedidos;
+import proyecto_final.tienda.service.PedidosService;
+
+
+public class PedidosController {
+	
+	@GetMapping("/pedidos")
+	public String index(Model model) {
+		model.addAttribute("lista_pedidos", PedidosService.getPedidos());
+		return "listado_pedidos";
+	}
+	
+	@GetMapping("/pedidos/registro")
+	public String getRegistro(Model model) {
+		
+		model.addAttribute("pedido", new Pedidos());
+		
+		return "pedidos_registro";
+	}
+	@PostMapping("/pedidos/registro/registrar")
+	public String registrar(Model model, @ModelAttribute Pedidos pedido) {
+		
+		PedidosService.addPedido(pedido);
+		
+		return "redirect:/";
+	}
+	
+	@GetMapping("/pedidos/delete")
+	public String borrar(Model model, @RequestParam String id) {
+		
+		PedidosService.deletePedido(PedidosService.getPedido(Integer.parseInt(id)));
+		
+		return "redirect:/";
+	}
+	
+	@GetMapping("/pedidos/edit")
+	public String editar(Model model, @RequestParam String id) {
+		
+		model.addAttribute("pedido", PedidosService.getPedido(Integer.parseInt(id)));
+		
+		return "productos_editar";
+	}
+	@PostMapping("/pedidos/edit/editar")
+	public String editSubmit(Model model, @ModelAttribute Pedidos pedido) {
+		
+		PedidosService.updatePedido(pedido);
+		
+		return "redirect:/";
+	}
+}
